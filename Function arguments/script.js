572a47e87858c12d006fd7a9d787e46f0fad70af
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-07 19:28:50 +0200

    Add files via upload

---
+++ Function arguments/script.js
@@ -0,0 +1,32 @@
+//No arguments....
+
+//const paintWalls = function () {
+  //  console.log ("The wall has been painted red");
+    
+//};
+
+//paintWalls();
+
+
+//single arguments....
+
+/*const paintWalls = function (item) {
+    console.log ("The wall has been painted " + item);
+    
+};*/
+
+//paintWalls("green");
+//paintWalls("Pink");
+//paintWalls("");
+
+//multiple arguments
+
+const paintWalls = function (houseWalls, paintColor){
+    console.log ("The " + houseWalls + " wall has been painted " + paintColor);
+    console.log ("The " + houseWalls + " wall has been painted " + paintColor);
+};
+
+paintWalls("north", "orange");
+paintWalls("southeast", "grey");
+
+
